5f237754a20b3a98ec7c90d63664fb7e16c9be67
Saltman0 <baudoin.mathieu@protonmail.com>
2026-01-29 23:23:05 +0100

    ✨ Ball updated

--- scenes/ball/Ball.cs
+++ scenes/ball/Ball.cs
@@ -1,16 +1,26 @@
 using Godot;
 using System;
 
-public partial class Ball : Area2D
+public partial class Ball : CharacterBody2D
 {
-	// Called when the node enters the scene tree for the first time.
+	[Export]
+	private float _speed = 500.0f;
+	
 	public override void _Ready()
 	{
+		Random random = new Random();
+		float randomX = (float)(random.NextDouble() * 2.0 - 1.0);
+		float randomY = (float)(random.NextDouble() * 2.0 - 1.0);
+		Velocity = new Vector2(randomX, randomY);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
-		Position += new Vector2(1000.0f, 0.0f) * (float)delta;
+		KinematicCollision2D collision = MoveAndCollide(Velocity * _speed * (float)delta);
+
+		if (collision != null)
+		{
+			Velocity = Velocity.Bounce(collision.GetNormal()) * 1.02f;
+		}
 	}
 }
