97e3a1b4cb509be18f800505fe6ba0ecaca989a1
Saltman0 <baudoin.mathieu@protonmail.com>
2026-01-31 12:45:18 +0100

    ✨ Game updated : scores updated + OnTimerTimeout TODOs added

--- scenes/game/Game.cs
+++ scenes/game/Game.cs
@@ -3,26 +3,45 @@ using System;
 
 public partial class Game : Node
 {
+	private int _leftScore;
+	private int _rightScore;
 	private Goal _goalLeft;
 	private Goal _goalRight;
+	private Timer _timer;
+	private Control _gameInterface;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_leftScore = 0;
+		_rightScore = 0;
 		_goalLeft = GetNode<Goal>("GoalLeft");
 		_goalRight = GetNode<Goal>("GoalRight");
+		_timer = GetNode<Timer>("Timer");
 
 		_goalLeft.GoalScored += OnGoalScored;
 		_goalRight.GoalScored += OnGoalScored;
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		_timer.Timeout += OnTimerTimeout;
 	}
 	
 	private void OnGoalScored(Goal goal)
 	{
-		GD.Print($"Goal scored in: {goal.Name}");
+		if (goal.Name.Equals("GoalLeft"))
+		{
+			_leftScore++;
+			GetNode<RichTextLabel>("GameInterface/Header/HBoxContainer/ScoreLeft").Text = _leftScore.ToString();
+		} else if (goal.Name.Equals("GoalRight"))
+		{
+			_rightScore++;
+			GetNode<RichTextLabel>("GameInterface/Header/HBoxContainer/ScoreRight").Text = _rightScore.ToString();
+		}
+	}
+
+	private void OnTimerTimeout()
+	{
+		GD.Print("Fini.");
+		// TODO Afficher le nom du vainqueur
+		// TODO Proposer de refaire une partie (tout réinitiliaser)
+		// TODO Revenir au menu principal
 	}
 }
