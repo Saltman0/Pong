1cdcca376159b42cd966f6de3adbabf146076518
Saltman0 <baudoin.mathieu@protonmail.com>
2026-02-01 13:04:18 +0100

    ✨ Game rules updated

--- scenes/game/Game.cs
+++ scenes/game/Game.cs
@@ -3,45 +3,65 @@ using System;
 
 public partial class Game : Node
 {
+	[Signal]
+	public delegate void ScoreUpdatedEventHandler(int newScore, string side);
+	
+	[Signal]
+	public delegate void TimeUpdatedEventHandler(int seconds);
+	
+	[Signal]
+	public delegate void GameOverEventHandler(string winner);
+
+	[Export]
+	private int _timeLeft;
+	[Export]
 	private int _leftScore;
+	[Export]
 	private int _rightScore;
-	private Goal _goalLeft;
-	private Goal _goalRight;
-	private Timer _timer;
-	private Control _gameInterface;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		_leftScore = 0;
-		_rightScore = 0;
-		_goalLeft = GetNode<Goal>("GoalLeft");
-		_goalRight = GetNode<Goal>("GoalRight");
-		_timer = GetNode<Timer>("Timer");
+		GetNode<Goal>("GoalLeft").GoalScored += OnGoalScored;
+		GetNode<Goal>("GoalRight").GoalScored += OnGoalScored;
+		GetNode<Timer>("Timer").Timeout += OnTimerTimeout;
+		ScoreUpdated += GetNode<GameInterface>("GameInterface").OnScoreUpdated;
+		TimeUpdated += GetNode<GameInterface>("GameInterface").OnTimeUpdated;
+		GameOver += GetNode<GameInterface>("GameInterface").OnGameOver;
 
-		_goalLeft.GoalScored += OnGoalScored;
-		_goalRight.GoalScored += OnGoalScored;
-		_timer.Timeout += OnTimerTimeout;
+		EmitSignalScoreUpdated(_leftScore, "left");
+		EmitSignalScoreUpdated(_rightScore, "right");
+		EmitSignalTimeUpdated(_timeLeft);
 	}
 	
 	private void OnGoalScored(Goal goal)
 	{
 		if (goal.Name.Equals("GoalLeft"))
 		{
-			_leftScore++;
-			GetNode<RichTextLabel>("GameInterface/Header/HBoxContainer/ScoreLeft").Text = _leftScore.ToString();
+			_rightScore++;
+			EmitSignalScoreUpdated(_leftScore, "right");
 		} else if (goal.Name.Equals("GoalRight"))
 		{
-			_rightScore++;
-			GetNode<RichTextLabel>("GameInterface/Header/HBoxContainer/ScoreRight").Text = _rightScore.ToString();
+			_leftScore++;
+			EmitSignalScoreUpdated(_leftScore, "left");
 		}
+		
 	}
 
 	private void OnTimerTimeout()
 	{
-		GD.Print("Fini.");
-		// TODO Afficher le nom du vainqueur
-		// TODO Proposer de refaire une partie (tout réinitiliaser)
-		// TODO Revenir au menu principal
+		_timeLeft--;
+		EmitSignalTimeUpdated(_timeLeft);
+		if (_timeLeft == 0)
+		{ 
+			if (_leftScore > _rightScore)
+			{
+				EmitSignalGameOver("left");
+			} else if (_rightScore > _leftScore) {
+				EmitSignalGameOver("right");
+			} else {
+				EmitSignalGameOver("none");
+			}
+		}
 	}
 }
