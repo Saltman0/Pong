0263f17fe034ff721b61bbeae6ef3b47cf4a6b32
Saltman0 <baudoin.mathieu@protonmail.com>
2026-02-18 02:23:08 +0100

    ♻️ Paddle script updated

--- scenes/paddle/Paddle.cs
+++ scenes/paddle/Paddle.cs
@@ -1,24 +1,33 @@
 using Godot;
 using System;
 
-public partial class Paddle : StaticBody2D
+public partial class Paddle : CharacterBody2D
 {
+	[Export]
+	private float _speed = 400.0f;
+	
+	[Export] 
+	private Color _color;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GetNode<Polygon2D>("Polygon2D").Color = _color;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
-		if (Input.IsActionPressed("move_up"))
-		{
-			Position += new Vector2(0.0f, -500.0f) * (float)delta;
-		}
-		
-		if (Input.IsActionPressed("move_down"))
-		{
-			Position += new Vector2(0.0f, 500.0f) * (float)delta;
-		}
+		Velocity = new Vector2(
+			0, 
+			Input.GetVector(
+				"move_left", 
+				"move_right", 
+				"move_up", 
+				"move_down"
+			).Y
+		) * _speed;
+
+		MoveAndSlide();
 	}
 }
