8334c3691f50dfd3781f40b5f76584b9712a2c2e
Saltman0 <baudoin.mathieu@protonmail.com>
2026-02-19 11:00:14 +0100

    ♻️ Paddle updated

--- scenes/paddle/Paddle.cs
+++ scenes/paddle/Paddle.cs
@@ -4,29 +4,23 @@ using System;
 public partial class Paddle : CharacterBody2D
 {
 	[Export]
-	private float _speed = 400.0f;
+	public float Speed = 400.0f;
 	
 	[Export] 
-	private Color _color;
+	public Color Color;
+
+	public float Direction;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GetNode<Polygon2D>("Polygon2D").Color = _color;
+		GetNode<Polygon2D>("Polygon2D").Color = Color;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
-		Velocity = new Vector2(
-			0, 
-			Input.GetVector(
-				"move_left", 
-				"move_right", 
-				"move_up", 
-				"move_down"
-			).Y
-		) * _speed;
+		Velocity = new Vector2(Velocity.X, Direction * Speed);
 
 		MoveAndSlide();
 	}
