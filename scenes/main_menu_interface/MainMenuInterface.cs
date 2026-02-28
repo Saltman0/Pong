b80ff3ccfdba1efdf4fca9cbb368d4606024f610
Saltman0 <baudoin.mathieu@protonmail.com>
2026-02-28 12:06:53 +0100

    ♻️ Main menu interface updated

--- scenes/main_menu_interface/MainMenuInterface.cs
+++ scenes/main_menu_interface/MainMenuInterface.cs
@@ -2,52 +2,30 @@ using Godot;
 
 public partial class MainMenuInterface : Control
 {
-	[Signal]
-	public delegate void SingleplayerButtonPressedEventHandler();
-	
-	[Signal]
-	public delegate void MultiplayerLocalButtonPressedEventHandler();
-	
-	[Signal]
-	public delegate void MultiplayerOnlineButtonPressedEventHandler();
-	
-	[Signal]
-	public delegate void SettingsButtonPressedEventHandler();
-	
-	[Signal]
-	public delegate void QuitButtonPressedEventHandler();
-	
 	public override void _Ready()
 	{
-		GetNode<Button>("VBoxContainer/SingleplayerButton").Pressed += OnSingleplayerButtonPressed;
-		GetNode<Button>("VBoxContainer/MultiplayerLocalButton").Pressed += OnMultiplayerLocalButtonPressed;
-		GetNode<Button>("VBoxContainer/MultiplayerOnlineButton").Pressed += OnMultiplayerOnlineButtonPressed;
-		GetNode<Button>("VBoxContainer/SettingsButton").Pressed += OnSettingsButtonPressed;
-		GetNode<Button>("VBoxContainer/QuitButton").Pressed += OnQuitButtonPressed;
-	}
-
-	public void OnSingleplayerButtonPressed()
-	{
-		EmitSignalSingleplayerButtonPressed();
-	}
-	
-	public void OnMultiplayerLocalButtonPressed()
-	{
-		EmitSignalMultiplayerLocalButtonPressed();
-	}
-	
-	public void OnMultiplayerOnlineButtonPressed()
-	{
-		EmitSignalMultiplayerOnlineButtonPressed();
-	}
-	
-	public void OnSettingsButtonPressed()
-	{
-		EmitSignalSettingsButtonPressed();
-	}
-	
-	public void OnQuitButtonPressed()
-	{
-		EmitSignalQuitButtonPressed();
+		VBoxContainer mainContainer = GetNode<VBoxContainer>("MainContainer");
+		
+		mainContainer.GetNode<Button>("SingleplayerButton").Pressed += () =>
+		{
+			Main.Instance.StartGame(Game.GameMode.Singleplayer);
+		};
+		mainContainer.GetNode<Button>("MultiplayerLocalButton").Pressed += () =>
+		{
+			Main.Instance.StartGame(Game.GameMode.LocalMultiplayer);
+		};
+		mainContainer.GetNode<Button>("MultiplayerOnlineButton").Pressed += () =>
+		{
+			SceneManager.Instance.SwitchScene(
+				GD.Load<PackedScene>(
+					"res://scenes/online_multiplayer_menu_interface/online_multiplayer_menu_interface.tscn"
+				).Instantiate<OnlineMultiplayerMenuInterface>()
+			);
+		};
+		mainContainer.GetNode<Button>("SettingsButton").Pressed += () =>
+		{
+			GD.Print("Settings button pressed !");
+		};
+		mainContainer.GetNode<Button>("QuitButton").Pressed += () => { GetTree().Quit(); };
 	}
 }
