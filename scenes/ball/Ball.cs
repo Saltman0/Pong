14916c77da590831256b2c84dd59348425f64b5c
Saltman0 <baudoin.mathieu@protonmail.com>
2026-02-28 17:51:06 +0100

    ✨ Ball updated

--- scenes/ball/Ball.cs
+++ scenes/ball/Ball.cs
@@ -2,26 +2,61 @@ using Godot;
 
 public partial class Ball : CharacterBody2D
 {
-	[Export]
-	private float _speed = 500.0f;
+	[Export] private float _speed = 500.0f;
+	[Export] public Vector2 SyncPosition { get; set; }
+	[Export] public Vector2 SyncVelocity { get; set; }
 	
 	public override void _Ready()
 	{
-		Launch();
+		if (!Multiplayer.HasMultiplayerPeer() || Multiplayer.IsServer())
+		{
+			Launch();
+		}
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		KinematicCollision2D collision = MoveAndCollide(Velocity * _speed * (float)delta);
-
-		if (collision != null)
+		bool isAuthority = !Multiplayer.HasMultiplayerPeer() || Multiplayer.IsServer();
+		if (isAuthority)
 		{
-			Velocity = Velocity.Bounce(collision.GetNormal()) * 1.02f;
+			KinematicCollision2D collision = MoveAndCollide(Velocity * _speed * (float)delta);
+
+			if (collision != null)
+			{
+				Velocity = Velocity.Bounce(collision.GetNormal());
+				if (collision.GetCollider() is Paddle)
+				{
+					Velocity *= 1.05f;
+				}
+			}
+
+			if (Multiplayer.HasMultiplayerPeer())
+			{
+				SyncPosition = GlobalPosition;
+				SyncVelocity = Velocity;
+			}
+		}
+		else
+		{
+			Velocity = SyncVelocity;
+			
+			KinematicCollision2D collision = MoveAndCollide(Velocity * _speed * (float)delta);
+			if (collision != null)
+			{
+				Velocity = Velocity.Bounce(collision.GetNormal());
+				if (collision.GetCollider() is Paddle)
+				{
+					Velocity *= 1.05f;
+				}
+			}
+			
+			GlobalPosition = GlobalPosition.Lerp(SyncPosition, (float)delta * 5.0f);
 		}
 	}
 
 	public void Launch()
 	{
 		Velocity = new Vector2(1.0f, 0.0f);
+		SyncVelocity = Velocity;
 	}
 }
