b70424100fcf99fa69208b49b4381978367f1e44
Saltman0 <baudoin.mathieu@protonmail.com>
2026-03-02 22:40:09 +0100

    ✨ Settings interface added

---
+++ scenes/settings_interface/SettingsInterface.cs
@@ -0,0 +1,56 @@
+using Godot;
+
+public partial class SettingsInterface : Control
+{
+	[Export] private OptionButton _windowModeOptionButton;
+	[Export] private OptionButton _resolutionOptionButton;
+	[Export] private OptionButton _vsyncOptionButton;
+	[Export] private Button _saveButton;
+	[Export] private Button _returnButton;
+
+	[Export] private HSlider _masterSlider;
+	[Export] private HSlider _musicSlider;
+	[Export] private HSlider _sfxSlider;
+	[Export] private HSlider _uiSlider;
+	
+	public override void _Ready()
+	{
+		_saveButton.Pressed += OnSaveButtonPressed;
+		_returnButton.Pressed += OnReturnButtonPressed;
+	}
+
+	private void OnSaveButtonPressed()
+	{
+		SaveVideoSettings();
+		SaveAudioSettings();
+	}
+	
+	private void OnReturnButtonPressed()
+	{
+		SceneManager.Instance.SwitchScene(
+			GD.Load<PackedScene>("res://scenes/main_menu_interface/main_menu_interface.tscn")
+				.Instantiate<MainMenuInterface>()
+		);
+	}
+
+	private void SaveVideoSettings()
+	{
+		DisplayServer.WindowSetFlag(DisplayServer.WindowFlags.MaximizeDisabled, false);
+		DisplayServer.WindowSetFlag(DisplayServer.WindowFlags.ResizeDisabled, false);
+		
+		VideoManager.Instance.SetWindowMode((DisplayServer.WindowMode)_windowModeOptionButton.GetSelectedId());
+		VideoManager.Instance.SetWindowScale(_resolutionOptionButton.GetSelectedId());
+		VideoManager.Instance.SetVsync((DisplayServer.VSyncMode)_vsyncOptionButton.GetSelectedId());
+		
+		DisplayServer.WindowSetFlag(DisplayServer.WindowFlags.MaximizeDisabled, true);
+		DisplayServer.WindowSetFlag(DisplayServer.WindowFlags.ResizeDisabled, true);
+	}
+	
+	private void SaveAudioSettings()
+	{
+		AudioManager.Instance.SetVolume("Master", (float)_masterSlider.Value);
+		AudioManager.Instance.SetVolume("Music", (float)_musicSlider.Value);
+		AudioManager.Instance.SetVolume("SFX", (float)_sfxSlider.Value);
+		AudioManager.Instance.SetVolume("UI", (float)_uiSlider.Value);
+	}
+}
