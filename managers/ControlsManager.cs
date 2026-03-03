fce67680fa3cb20148a6519efe9950ed9dd0735e
Saltman0 <baudoin.mathieu@protonmail.com>
2026-03-03 17:27:16 +0100

    ✨ Controls manager updated

--- managers/ControlsManager.cs
+++ managers/ControlsManager.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace Pong.managers;
 
 using Godot;
@@ -5,6 +7,19 @@ using Godot;
 public static class ControlsManager
 {
 	private const string SavePath = "user://settings.cfg";
+	
+	public static void SaveDefaultControls()
+	{
+		InputEvent moveUpP1InputEvent = InputMap.ActionGetEvents("move_up").First();
+		InputEvent moveDownP1InputEvent = InputMap.ActionGetEvents("move_down").First();
+		InputEvent moveUpP2InputEvent = InputMap.ActionGetEvents("move_up_2").First();
+		InputEvent moveDownP2InputEvent = InputMap.ActionGetEvents("move_down_2").First();
+		
+		SaveControl("move_up", moveUpP1InputEvent);
+		SaveControl("move_down", moveDownP1InputEvent);
+		SaveControl("move_up_2", moveUpP2InputEvent);
+		SaveControl("move_down_2", moveDownP2InputEvent);
+	}
 
 	public static void SaveControl(string actionName, InputEvent inputEvent)
 	{
@@ -14,10 +29,10 @@ public static class ControlsManager
 		config.Save(SavePath);
 	}
 	
-	public static void LoadControls()
+	public static bool LoadControls()
 	{
 		ConfigFile config = new ConfigFile();
-		if (config.Load(SavePath) != Error.Ok) return;
+		if (config.Load(SavePath) != Error.Ok) return false;
 		
 		foreach (string action in config.GetSectionKeys("Controls"))
 		{
@@ -25,5 +40,7 @@ public static class ControlsManager
 			InputMap.ActionEraseEvents(action);
 			InputMap.ActionAddEvent(action, inputEvent);
 		}
+
+		return true;
 	}
 }
